ca472efd22b23bb224608743727a6bfc76e1af4e
Quentin Crida <qcrida@gmail.com>
2019-11-12 09:03:14 +0000

    added to team array

--- team/specs/team_spec.rb
+++ team/specs/team_spec.rb
@@ -22,6 +22,16 @@ class TeamTest < MiniTest:: Test
     assert_equal('Rassie Erasmus', team.get_coach)
   end
 
+  def test_set_coach
+    team = Team.new('Springboks', ['Sipho', 'Faf, Mapimpi', 'Cheslin'], 'Rassie Erasmus')
+    team.set_coach('Peter de Villiers')
+    assert_equal('Peter de Villiers', team.get_coach)
+  end
 
+  def test_add_player
+    team = Team.new('Springboks', ['Sipho', 'Faf, Mapimpi', 'Cheslin'], 'Rassie Erasmus')
+    assert_equal(['Sipho', 'Faf, Mapimpi', 'Cheslin', 'Handre'], team.add_player('Handre'))
+
+  end
 #
 end
--- team/team.rb
+++ team/team.rb
@@ -1,10 +1,12 @@
 class Team
+  attr_accessor :team_name, :players, :coach
 
 
   def initialize(input_team_name, input_players, input_coach)
     @team_name = input_team_name
     @players = input_players
     @coach = input_coach
+
 end
 
   def get_name
@@ -17,6 +19,14 @@ end
 
   def get_coach
     return @coach
+  end
+
+  def set_coach(coach)
+    @coach = coach
+  end
+
+  def add_player(player)
+    @players.push(player)
   end
 #
 end
